b3a00bc0e48f39a8e59bad9c04fd4e1f875f58f7
Eivin Landa <landa@drivdigital.no>
2014-10-27 06:50:28 +0100

    First working draft

---
+++ docgen.php
@@ -0,0 +1,190 @@
+<?php
+
+// JSON Doc
+header('Content-Type: application/json');
+
+$path = '../VoidJS';
+$ext = 'js';
+$save_to = 'void-js';
+
+// Files to ignore (regex):
+$ignore = array(
+  'node_modules',
+  '^\.'
+);
+// A class for code documentation
+class CodeDoc {
+  var $valid = FALSE;
+  var $file_name;
+  var $line;
+  var $title;
+  var $definition;
+  var $parameters;
+  public function __construct( $content, $definition, $file_name='', $pos='' ) {
+    $lines = explode( "\n", $content );
+    array_shift( $lines );
+    array_pop( $lines );
+    foreach ( $lines as &$line ) {
+      if ( !preg_match( '/\s+\*/', $line ) ) {
+        return FALSE;
+      }
+      $line = preg_replace( '/\s+\*\s*/', '', $line );
+    }
+    $this->valid = TRUE;
+    $this->file_name = $file_name;
+    $this->line = $pos;
+    if ( $definition ) {
+      $this->definition = $definition['name'];
+
+      $this->parameters = explode( ',', str_replace(' ', '', $definition['params'] ) );
+    }
+    $this->title = array_shift( $lines );
+
+    $description = array();
+    foreach ($lines as $line) {
+      if ( strlen($line) && '@' === $line[0] ) {
+        break;
+      }
+      $description[] = $line;
+    }
+    $this->description = implode( "\n", $description );
+  }
+
+  public function to_array() {
+    return array(
+      'title' => $this->title,
+      'description' => $this->description,
+      'definition' => $this->definition,
+      'parameters' => $this->parameters,
+      'file' => $this->file_name,
+      'line' => $this->line,
+    );
+  }
+
+}
+// A factory class for CodeDoc
+class CodeDir {
+  var $ext = array();
+  var $ignore;
+  var $code_docs = array();
+  public function __construct( $ext, $ignore = array() ) {
+    $this->ignore = $ignore;
+    if ( is_array( $ext ) ) {
+      $this->ext = $ext;
+    }
+    elseif ( is_string( $ext ) ) {
+      $this->ext = explode(',', $ext);
+    }
+    else {
+      throw new Exception("Extension must be array or string", 1);
+    }
+  }
+
+  public function traverse_dir( $dir ) {
+    // Scan the dir
+    $scan = scandir( $dir );
+    // Iterate through the scan
+    foreach ( $scan as $path ) {
+      if ( '.' === $path || '..' === $path ) {
+        continue;
+      }
+
+      // Ignore ignored files
+      foreach ($this->ignore as $ignore) {
+        $cont = false;
+        if ( preg_match( "/{$ignore}/", $path ) ) {
+          $cont = true;
+          break;
+        }
+        if ( $cont ) {
+          continue;
+        }
+      } // - ignore
+      // Recursive search for files
+      if ( is_dir( $dir .'/'.$path ) ) {
+        $this->traverse_dir( $dir .'/'. $path );
+      }
+      // Check file for extension
+      else {
+        $ext = pathinfo( $dir .'/'.$path, PATHINFO_EXTENSION );
+        if ( in_array( $ext, $this->ext ) ) {
+          // Process the file
+          $this->process_file( $dir .'/'. $path );
+        }
+      }
+    }
+    return $this;
+  }
+
+  public function process_file( $file ) {
+    $content = file_get_contents( $file );
+    $pos = -1;
+    do {
+      $pos = strpos( $content, "/**\n", $pos + 1 );
+      if ( FALSE !== $pos ) {
+        $end_pos  = strpos( $content, '*/', $pos );
+        if ( FALSE !== $end_pos ) {
+          // Get the code header
+          $header = substr( $content, $pos, $end_pos - $pos + 2 );
+          // Get the source code
+          $def = $this->get_def( $content, $end_pos );
+          $doc = new CodeDoc( $header, $def, $file, $pos );
+          if ( $doc->valid ) {
+            $id = count( $this->code_docs );
+            $this->code_docs[] = $doc;
+          }
+          else {
+            $this->errors[] = $file .':'. $pos;
+          }
+        }
+      }
+    } while ( FALSE !== $pos );
+
+    return $this;
+  }
+
+  function get_def( $content, $pos ) {
+    $new_line = strpos( $content, "\n", $pos );
+    // Sanity check position
+    if ( FALSE === $new_line ) { return ''; }
+    // Sanity check position
+    $new_line_end = strpos( $content, "\n", $new_line + 1 );
+    if ( FALSE === $new_line_end ) { return ''; }
+
+    // Get the line
+    $line = substr( $content, $new_line,  $new_line_end - $new_line );
+
+    $matches = array();
+
+    // PHP style functions
+    if ( preg_match( '/^\s*function\s+([^\(]+)\(([^\)]+)\)\s*\{/', $line, $matches ) ) {
+      return array(
+        'name'   => $matches[1],
+        'params' => $matches[2],
+      );
+    }
+    // JavaScript style functions
+    if ( preg_match( '/^\s*(?:var\s+)?(\S+)\s*=\s*function\s*\(([^\)]+)\)\s*\{/', $line, $matches ) ) {
+      return array(
+        'name'   => $matches[1],
+        'params' => $matches[2],
+      );
+    }
+    return FALSE;
+  }
+
+  public function export() {
+    $docs = array();
+    foreach ( $this->code_docs as $code_doc ) {
+      $docs[] = $code_doc->to_array();
+    }
+    return $docs;
+  }
+
+}
+
+
+$code_dir = new CodeDir( $ext, $ignore );
+$docs = $code_dir->traverse_dir( $path )->export();
+
+echo json_encode( $docs );
---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Voidjs Docs</title>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.js" type="text/javascript"></script>
+  <link href="http://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.css" type="text/css">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <h1>Menu</h1>
+      <nav id="menu"></nav>
+    </div>
+    <div class="col-md-8">
+      <div class="page-header">
+        <h1>Documentation</h1>
+      </div>
+      <div id="content"></div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+var files = {};
+var build_menu = function() {
+  var menu = $('#menu');
+  for ( var file in files ) {
+    menu.append(
+      $('<strong>').text( file )
+    );
+    file_docs = $('<ul>');
+
+    for ( var title in files[file] ) {
+      var doc = files[file][title];
+      console.log(doc);
+      var list_item = $('<li>').append(
+        $('<a>').text( title )
+          .attr({
+            'href': '#'+ doc.id
+          })
+      );
+
+      file_docs.append( list_item );
+    }
+    menu.append(
+      file_docs
+    );
+  }
+};
+var content, id_index = 0;
+var process_doc = function( doc ) {
+  var def = doc.definition || doc.title;
+  var title = doc.title || doc.definition;
+  if ( !files[doc.file] ) { files[doc.file] = {}; }
+  var object = {
+    doc:doc,
+    elem: {},
+    id: ++id_index,
+  };
+  var box = $('<article>');
+  box.appendTo( content );
+  if ( title ) {
+    object.elem.title = $('<h2>');
+    object.elem.title
+      .text( title )
+      .attr( 'id', id_index )
+      .appendTo( box );
+  }
+  if ( doc.file ) {
+    object.elem.file = $('<small>');
+    object.elem.file
+      .addClass( 'cf' )
+      .text( doc.file + ( doc.line ? ':' + doc.line : '' ) )
+      .appendTo( box );
+  }
+  // Progress bar
+  var progress = doc.progress;
+  if ( !progress ) { progress = 0; }
+  object.elem.progress = $('<div>');
+  object.elem.progress
+    .addClass('progress')
+    .append( $('<div>')
+      .addClass('progress-bar')
+      .attr({
+        role: 'progress',
+        'aria-valuenow': progress,
+        'aria-valuenowmin': 0,
+        'aria-valuenowmax': 100,
+      })
+    )
+    .appendTo( box );
+
+  if ( 15 > progress ) {
+    object.elem.progress
+      .addClass('progress-bar-danger');
+  }
+  else if ( 30 > progress ) {
+    object.elem.progress
+      .addClass('progress-bar-warning');
+  }
+  else if ( 60 < progress ) {
+    object.elem.progress
+      .addClass('progress-bar-success');
+  }
+
+  if ( doc.definition ) {
+    object.elem.definition = $('<code>');
+    object.elem.definition
+      .text( doc.definition )
+      .appendTo( box );
+  }
+  if ( doc.parameters ) {
+    // @TODO: Recursive <dl>
+    object.elem.parameters = $('<div>');
+    object.elem.parameters
+      .text( doc.parameters )
+      .appendTo( box );
+  }
+  if ( doc.description ) {
+    object.elem.description = $('<p>');
+    object.elem.description
+      .html( doc.description.replace("\n", '<br>') )
+      .appendTo( box );
+  }
+  object.elem.box = box;
+  files[doc.file][def] = object;
+};
+
+var fill_data = function( json ) {
+  console.log( json );
+  for (var i = 0; i < json.length; i++) {
+    process_doc( json[i] );
+  }
+
+  build_menu();
+};
+
+$(document).ready(function() {
+  content = $('#content');
+  $.getJSON( '/docgen.php', fill_data );
+});
+</script>
+</body>
+</html>
